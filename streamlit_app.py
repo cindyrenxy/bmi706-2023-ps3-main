a759d03158fa1d9c4bd914bb9b899a0a2fb24e10
Cindy Ren <xren15@jhu.edu>
2025-11-11 22:00:14 -0500

    added scale domain q2.5

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,7 +105,7 @@ ages = [
 heatmap = alt.Chart(subset).mark_rect().encode(
     x=alt.X("Age:O", sort=ages),
     y=alt.Y("Country:N"),
-    color=alt.Color("Rate:Q", title="Mortality rate per 100k"),
+    color=alt.Color("Rate:Q", title="Mortality rate per 100k", scale=alt.Scale(domain=[0.01, 100])),
     tooltip=["Country", "Age", alt.Tooltip("Rate:Q", format=".2f")]
 ).properties(
     title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
